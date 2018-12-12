51d62577805f34318cfd0b5003ea5f482397513e
JARBerry <rayberry@rays-macbook.local>
2018-12-12 11:27:49 +0000

    Created with Safari and photo library added

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -2,19 +2,85 @@
 //  ViewController.swift
 //  SystemViewControllers
 //
-//  Created by Ray Berry on 11/12/2018.
+//  Created by James and Ray Berry on 11/12/2018.
 //  Copyright © 2018 JARBerry. All rights reserved.
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+        if let url = URL(string: "http://www.apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        // Add Privacy - Camera Usage Description to Info.plist
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+          let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true, completion: nil)
+             })
+             alertController.addAction(cameraAction)
+        }
+        
+       // Add "Privacy - Photo Library Usage Description" to info.plist
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)})
+             alertController.addAction(photoLibraryAction)
+            
+        }
 
+        
+        alertController.popoverPresentationController?.sourceView = sender
+        present(alertController, animated: true, completion:  nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = imageSelected
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
